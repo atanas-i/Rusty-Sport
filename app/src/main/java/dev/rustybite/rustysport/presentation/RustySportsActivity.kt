7d4f9442f05fc1b953dc42e664da7d6745ccba1b
atanas-i <atanasi.dev@gmail.com>
2023-12-28 13:05:11 +0300

    add classes and annotations required by hilt dependency injection

--- app/src/main/java/dev/rustybite/rustysport/presentation/RustySportsActivity.kt
+++ app/src/main/java/dev/rustybite/rustysport/presentation/RustySportsActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import dev.rustybite.rustysport.presentation.navigations.RustySportsNavHost
 import dev.rustybite.rustysport.ui.theme.RustySportTheme
 
+@AndroidEntryPoint
 class RustySportsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
